6ce68af5c0ac55d45d5ee7a16c1538ab6b9027f1
Fábio Filho <fabio.filhossi@gmail.com>
2020-08-26 17:12:35 -0300

    Fix settings

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='pareto_reports',
+  packages=['pareto_reports'],
+  version='0.0.1',
+  description='Pareto Report Definition',
+  author='Pareto Group',
+  author_email='noreply@paretogroup.com.br',
+  url='https://github.com/paretogroup/pareto-reports',
+  keywords=['pareto', 'api', 'reports'],
+  classifiers=[],
+)
