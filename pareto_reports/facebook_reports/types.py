e9ea5c5c83f2e6f2e0e7cc5441652198bcadd20f
Fábio Filho <fabio.filhossi@gmail.com>
2020-08-31 14:24:06 -0300

    [#3] Add support for Facebook reporting

--- pareto_reports/facebook_reports/types.py
+++ pareto_reports/facebook_reports/types.py
@@ -19,5 +19,6 @@ class FacebookAdsAccountPerformanceReportType(FacebookAdsReportType):
     date = FacebookAttributeReportField(target_name="date_start")
     date_start = FacebookAttributeReportField(target_name="date_start")
     date_end = FacebookAttributeReportField(target_name="date_end")
+    currency = FacebookMetricReportField(target_name="account_currency")
     cost = FacebookMetricReportField(target_name="spend")
     ad_set_id = FacebookMetricReportField(target_name="id", target_group_name="adsets")
--- temp.py
+++ temp.py
@@ -10,7 +10,7 @@ from pareto_reports.facebook_reports import FacebookAdsReportTypeResolver
 FACEBOOK_BASE_API = "https://graph.facebook.com"
 FACEBOOK_VERSION_API = "v7.0"
 
-TEMP_ACCESS_TOKEN = "EAAIuoYA4YdIBAFusU2tddI0BtXX5uXltFJc3ZBYX81MkFODo8PDZCAvW09HsAPkbrRMxIojOC5dZBMdstaM2QQwcZA6PdnQruoyZCX9M4sZBAcmce2R8hU3X1bZCouuwnAHZCsC78WxcmZCiOUL9p99Vuemq5ZC9xYiCGqCZBt5dMbdInVjNpVZCAUvXPQDdNL7PhnxZCIkXSg1b1b7DzIXE7dZBcukIoYw6OHZANjje5pzwwqHhMHrbz58xCLvy3VARFihFhIZD"
+TEMP_ACCESS_TOKEN = "EAAIuoYA4YdIBAAByxwPQrG97tosgc0ZBP3AadLZCcGZCXp2rBflbuo6cNoWXzwviMZA3O9sPwnzKXJ9k1d1tsxVy5ziMuYBZA3PDQb3X36wZBlHHoJm458hyRAQ0klcmMFYVWR0JVKT2Bskv8f40GK2K4uPbjxEgGVGOWrp9LwpUmOxk7jZAJZANxbqBBDh5pKbkSrkEufd1IH39KWpNwZCfi"
 
 FACEBOOK_USER_INFO_REPORT_ENDPOINT = 'me'
 FIELDS = "id,name,accounts{name}"
@@ -25,9 +25,7 @@ def get_base_url():
     return f"{FACEBOOK_BASE_API}/{FACEBOOK_VERSION_API}"
 
 
-async def request(
-):
-
+async def request():
     async with ClientSession() as session:
         client = ReportClient(FacebookAdsReportTypeResolver, {
             'FACEBOOK_TOKEN': TEMP_ACCESS_TOKEN,
@@ -38,17 +36,16 @@ async def request(
             "report_type": "FACEBOOK_ADS_ACCOUNT_PERFORMANCE_REPORT",
             "report_name": "test",
             "selector": {
-                "fields": ["cost", "date_start", "date_end"],
+                "fields": ["cost", "date", "currency"],
                 "date_range": {
                     "start": "2020-02-15",
                     "end": "2020-02-16",
-                    "time_increment": 2
                 },
                 # "predicates": [
                 #     {
                 #         "field": "customer_id",
                 #         "operator": "equals",
-                #         "values": ["TRUE"]
+                #         "values": ["2061402793900152"]
                 #     }
                 # ]
             }
