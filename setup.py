14dfd5272f9d8dca71a9af3a7e79cb94fa233ddf
Fábio Filho <fabio.filhossi@gmail.com>
2020-09-02 18:31:46 -0300

    Update setup config

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
   name='omni_reports',
   packages=['omni_reports'],
-  version='0.0.1',
+  version='0.0.3',
   description='Omni Report Definition',
   author='Pareto Group',
   author_email='noreply@paretogroup.com.br',
-  url='https://github.com/paretogroup/pareto-reports',
+  url='https://github.com/paretogroup/omni-reports',
   keywords=['pareto', 'api', 'reports'],
   classifiers=[],
 )
